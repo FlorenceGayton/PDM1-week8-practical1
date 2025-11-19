d7d2a9ab97c017f44f1d0e91ecf7ad40e862af8d
Flo :) <103675778+FlorenceGayton@users.noreply.github.com>
2025-11-19 11:31:07 +0000

    Add sound effect for circle boundary collisions

--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -3,6 +3,12 @@ let y = 50;
 let speedX = 3;
 let speedY = 3;
 
+function preload(){
+    soundFormats('wav');
+    boing = loadSound('assets/159376__greenhourglass__boing1.wav');
+}
+
+
 function setup() {
     createCanvas(600, 400);
 }
@@ -11,9 +17,11 @@ function draw() {
     background(255);
     circle(x, y, 100);
     if (x < 50 || x > width - 50) {
+        boing.play();
         speedX *= -1;
     }
     if (y < 50 || y > height - 50) {
+        boing.play();
         speedY *= -1;
     }
     x += speedX;
